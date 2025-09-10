116a1e744467e1bcd403a2dc3c2e45558ec6ed53
Ekin Nohutcu <ekinnohutc@gmail.com>
2025-09-10 18:01:39 +0300

    device simulator impoted

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -7,13 +7,14 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
